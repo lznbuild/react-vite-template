30bcd3f6c09839699f976653d9489c92b3d03775
lznbuild <lznbuild@163.com>
2021-09-15 10:18:56 +0800

    test

--- src/components/Example/index.tsx
+++ src/components/Example/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import cn from 'classnames';
+import { useEffect, useState } from 'react';
 
 import styles from './index.module.less';
 
@@ -8,11 +10,22 @@ export interface ExampleProps {
 }
 
 export function Example({ className, title = 'Example' }: ExampleProps) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      console.log('render');
+      
+      setCount((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  
   return (
     <div className={cn(styles.container, className)}>
-      <header className={styles.header}>
-        <h1 className={styles.title}>{title}</h1>
-      </header>
+      <button type="button" onClick={()=> setCount(count+1)}>set num</button>
+      {count}
     </div>
   );
 }
--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,5 +1,25 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-alert */
+import { useState } from "react";
 import { Example } from '../Example';
 
+
 export function Root() {
-  return <Example title="Hello, World!" />;
+  const [count, setCount] = useState(0);
+
+  function handleAlertClick() {
+    setTimeout(() => {
+      alert(`You clicked on: ${count}`);
+    }, 3000);
+  }
+  return <div>
+    <p>You clicked {count} times</p>
+    <button onClick={() => setCount(count + 1)}>
+      Click me
+    </button>
+    <button onClick={handleAlertClick}>
+      Show alert
+    </button>
+    <Example />
+  </div>
 }
